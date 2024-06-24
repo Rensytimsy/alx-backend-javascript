cb73d9c81af8e106b1e809bc51d43e141cf3dda9
Rensytimsy <rensytimsy03@gmail.com>
2024-06-24 20:43:43 +0300

    5-spread should be one line lonnggggg!!!!

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,8 +1,1 @@
-export default function concatArrays(array1, array2, string) {
-    //Welcome to the wonders of spread operators.
-    /**
-     * Basically this concept is to enable one see the possibility of using array spread operator to access values
-     */
-
-    return [...array1, ...array2, ...string];
-}
+export default function concatArrays(array1, array2, string) {return [...array1, ...array2, ...string];}
