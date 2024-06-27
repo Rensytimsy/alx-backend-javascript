6349af68479952f722ccb0489ea38c78fe91b704
Rensytimsy <rensytimsy03@gmail.com>
2024-06-28 01:00:21 +0300

    all

--- 0x02-ES6_classes/4-main.js
+++ 0x02-ES6_classes/4-main.js
@@ -1,5 +1,5 @@
 import Pricing from './4-pricing';
-import Currency from './3-currency';
+import Currency from './3-currency5-building.js';
 
 const p = new Pricing(100, new Currency('EUR', 'Euro'));
 console.log(p);
