7a4382d56cba8572268088e201a3469494b065fa
Rensytimsy <rensytimsy03@gmail.com>
2024-06-28 17:23:16 +0300

    eslint errors

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -6,6 +6,6 @@ export default function taskBlock(trueOrFalse) {
     const task = true; // eslint-disable-line no-unused-vars
     const task2 = false; // eslint-disable-line no-unused-vars
   }
-  
+
   return [task, task2];
 }
