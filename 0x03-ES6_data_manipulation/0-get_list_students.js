10a033f33ac344db1edb7ea9895e1a01e57528ef
Rensytimsy <rensytimsy03@gmail.com>
2024-07-01 21:06:24 +0300

    an array of lists

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+const studentsArray = [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+function getListStudents() {
+  return [...studentsArray];
+}
+export default getListStudents;
