3543d85b06943415dc82df964a25dceb6d702efa
Rensytimsy <rensytimsy03@gmail.com>
2024-08-21 21:20:29 +0300

    simple server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,20 @@
-const {createServer} = require('node:http');
+const http = require('http');
 
-const port = 1245;
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
-const app = createServer((req, res) => {
-    res.writeHead(200, 'Content-type', 'text/plain')
-    res.write("Hello Holberton School!")
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-app.listen(port, () => console.log(`https://localhost:${port}`))
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 module.exports = app;
